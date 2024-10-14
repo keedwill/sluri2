2581a8ab06ea9cb2689589e0d6dd9ff2d5548138
owoh princewill <princewillowoh@gmail.com>
2024-10-14 14:46:02 +0100

    fixing of bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from "react";
-import { IoArrowForward } from "react-icons/io5";
+
 
 const About = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-    
   }, []);
   return (
     <div className=" flex-col flex ">
-      <div className="clip-path-circle overflow-x-clip bg-gradient-to-br from-primary to-[#91bea4] h-[200px] text-secondary md:text-[54px] text-[40px] pl-[5%] items-center md:justify-normal justify-center flex font-bold">
+      <div
+        id="circular"
+        className="clip-path-circle overflow-x-clip bg-gradient-to-br from-primary to-[#91bea4] h-[200px] text-secondary md:text-[60px] text-[40px] pl-[5%] items-center md:justify-normal justify-center flex font-bold"
+      >
         ABOUT SLURI
       </div>
       <div className="m-[5%]  grid  gap-12 md:gap-0  grid-cols-1 text-secondary">
         <div className="flex flex-col gap-4">
-          <h5 className="text-lg font-bold">
+          <h5 id="circular" className="text-[40px] leading-[40px]">
             {" "}
             Your Natural Wellness Boost, A Taste of Nature, A Boost for Your
             Health
@@ -33,113 +35,94 @@ const About = () => {
                 benefits.
               </p>
             </div>
-            <div>
-              <h5 className="text-lg font-bold">
+            <div className="">
+              <p
+                id="circular"
+                className="text-[50px] leading-[50px] text-center my-6  "
+              >
                 {" "}
                 Discover the Power of Sluri
-              </h5>
+              </p>
             </div>
-            <div className=" w-full grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-              <div className="border-2 flex justify-center items-center  border-primary">
+            <div className=" w-full grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 justify-items-center  ">
+              <div className=" shadow-2xl  shadow-primary  flex text-sm justify-center skew-y-3   hover:scale-105 transition-transform duration-300 ease-in-out   items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px] h-[200px]">
                 <span>Enhanced Libido and Sexual Desire</span>
               </div>
-              <div className="flex items-center gap-2 ">
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>Ease bowel movements and support digestive health.</span>
               </div>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              <div className="shadow-2xl  shadow-primary  flex text-sm -skew-x-3 justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px] h-[200px]">
                 <span>
                   {" "}
                   Enjoy a comfortable and pleasurable intimate experience.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm skew-y-3 justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   Flush out toxins and impurities from your body, promoting
                   optimal health.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Experience a surge in sexual desire and heightened arousal.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm -skew-x-3 justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Help regulate blood sugar levels and support overall metabolic
                   health.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center skew-y-3 drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out   items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Protect your cells from damage and boost your immune system.
                 </span>
-              </p>
+              </div>
 
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Promote hormonal equilibrium and support reproductive health.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm -skew-x-3 justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Sulri's delightful sweet taste makes it a perfect addition to
                   your favorite beverages.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center skew-y-3 drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out   items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Enjoy it with alcohol, milk, smoothies, fruit drinks, or
                   simply on its own.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Ditch artificial sodas and embrace a healthier lifestyle with
                   Sulri.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm -skew-x-3 justify-center drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out    items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Indulge in the natural goodness of Sulri and discover a world
                   of health benefits.
                 </span>
-              </p>
-              <p className="flex items-center gap-2 ">
-                {" "}
-                <IoArrowForward />
+              </div>
+              <div className="shadow-2xl  shadow-primary  flex text-sm justify-center skew-y-3 drop-shadow-lg  hover:scale-105 transition-transform duration-300 ease-in-out   items-center text-center p-2  border-2 border-primary rounded-3xl w-[100%] md:w-[200px]  h-[200px]">
                 <span>
                   {" "}
                   Join us on a journey towards a healthier, happier you.
                 </span>
-              </p>
+              </div>
             </div>
           </div>
         </div>
